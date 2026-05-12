410fa27110fc509a9ecb79fb842243b717c7799c
Eliexer Guevara <66179848+eliexerguevara@users.noreply.github.com>
2026-05-12 16:53:38 -0300

    update

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Updating Whaticket-Exacom, please wait."
+
+cd ~
+cd whaticket-exacom
+git pull
+cd backend
+npm install
+rm -rf dist
+npm run build
+npx sequelize db:migrate
+npx sequelize db:seed
+cd ../frontend
+npm install
+rm -rf build
+npm run build
+pm2 restart all
+
+echo "Update finished. Enjoy!"
